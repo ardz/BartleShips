2af3a1cece03ab1987b4cf7f0b974083d9ce696a
Richard Peckett <richard.peckett@outlook.com>
2018-02-13 18:28:48 +0000

    added csproj and sln

---
+++ BartleShips/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BartleShips
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
