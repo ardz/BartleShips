11b0c2c9103b36b22462d11806a05c76d822b124
Richard Peckett <richard.peckett@outlook.com>
2018-02-13 18:54:05 +0000

    added vessel

---
+++ BartleShips/Vessel/Vessel.cs
@@ -0,0 +1,14 @@
+﻿namespace BartleShips.Vessel
+{
+    public abstract class Vessel
+    {
+        public string Name { get; set; }
+        public int Length { get; set; }
+        public int Hits { get; set; }
+
+        // the first => is an expression body in c# (replaces get { return })
+        // if the number of hits is greater than or equal to the length of
+        // the vessel then return true (ship is sunk)
+        public bool IsSunk => Hits >= Length;
+    }
+}
