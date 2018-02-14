708299fcb6a06752b0cee705fd6c48fa71c118e0
Richard Peckett <richard.peckett@outlook.com>
2018-02-14 13:49:37 +0000

    added coordinate

---
+++ BartleShips/Board/Coordinate.cs
@@ -0,0 +1,14 @@
+﻿namespace BartleShips.Board
+{
+   public class Coordinate
+    {
+        public int Row { get; set; }
+        public int Column { get; set; }
+
+        public Coordinate(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
+    }
+}
